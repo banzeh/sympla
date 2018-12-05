8905cc87dfdfcee071082c78f172755cdd0dce74
Flavio Takeuchi <flavio@banzeh.com.br>
2018-12-05 12:14:44 -0200

    parse para a classe de Links

--- src/model/link.ts
+++ src/model/link.ts
@@ -5,7 +5,14 @@ export class Link {
 
   constructor(method: string, rel: string, href: string){
     this.method = method;
-    this.rel = rel;
-    this.href = href;
+    this.rel    = rel;
+    this.href   = href;
+  }
+
+  parse(result: any): Link {
+    this.method = result.method;
+    this.rel    = result.rel;
+    this.href   = result.href;
+    return this;
   }
 }
