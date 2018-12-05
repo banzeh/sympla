70e06c74bff76ee2118a04a44d6f170e2b526be9
Flavio Takeuchi <flavio@banzeh.com.br>
2018-12-05 12:01:26 -0200

    Correção de testes

--- src/erede.spec.ts
+++ src/erede.spec.ts
@@ -1,6 +1,5 @@
 import { ERede } from './erede'
 import { expect } from 'chai';
-import 'mocha';
 import * as randomstring from 'randomstring'
 import { AuthorizationResponse } from './model/authorizationResponse';
 import { CaptureResponse } from './model/captureResponse';
@@ -8,54 +7,58 @@ import { RefundResponse } from './model/refundResponse';
 
 var erede = new ERede("10000980", "b884f83537a441e7ab96cbfd9a76fa8f", true);
 
-function cancelaVenda(resultCapture: CaptureResponse): Promise<RefundResponse> {
-  return new Promise((resolve, reject) => {
-    const cancelSaleParams = {
-      "amount": 2099
-    };
+const authorizationParams = {
+  "capture": false,
+  "reference": randomstring.generate(15),
+  "amount": 2099,
+  "cardholderName": "John Snow", 
+  "cardNumber": "5448280000000007",
+  "expirationMonth": 12, 
+  "expirationYear": 2020, 
+  "securityCode": "235"
+};
 
-    return erede.cancelSale(resultCapture.tid, cancelSaleParams)
-    .then((resultCancel) => {
-      expect(resultCancel.returnCode).to.be.oneOf(['359','360']);
-      resolve(resultCancel)
-    })
-    .catch(reject)
+describe('Teste SDK E-Rede', () => {
+  let authorization = erede.authorization(authorizationParams);
+  let captura       = authorization.then(capturaVenda);
+  let cancelamento  = captura.then(cancelaVenda);
+
+  it('Teste de Autorização da Transação', async function() {
+    this.timeout(0);
+    let resultAuthorization = await authorization;
+    expect(resultAuthorization.reference).to.be.equal(authorizationParams.reference);
+    expect(resultAuthorization.returnMessage).to.be.equal('Success.');
+
   })
-}
+
+  it('Teste de Captura da Transação', async function() {
+    this.timeout(0);
+    let captureResponse = await captura
+    expect(captureResponse.returnMessage).to.be.equal('Success.');
+    expect(captureResponse.returnCode).to.be.equal('00');
+  })
+
+  it('Teste de Cancelamento da Transação', async function() {
+    this.timeout(0);
+    let refundResponse = await cancelamento;
+    expect(refundResponse.returnCode).to.be.oneOf(['359','360']);
+    expect(refundResponse.returnMessage).to.be.equal('Refund successful.');
+  })
+})
+
+
+
 
 function capturaVenda(resultAuthorization: AuthorizationResponse): Promise<CaptureResponse> {
   const captureParams = {
     amount: 2099
   };
-
   return erede.capture(resultAuthorization.tid, captureParams);
 }
 
-describe('E-Rede sdk nodejs', () => {
-  it('should return base64 credetial token', function (done) {
-    this.timeout(0);
-
-    const authorizationParams = {
-      "capture": false,
-      "reference": randomstring.generate(15),
-      "amount": 2099,
-      "cardholderName": "John Snow", 
-      "cardNumber": "5448280000000007",
-      "expirationMonth": 12, 
-      "expirationYear": 2020, 
-      "securityCode": "235"
-    };
-  
-    erede.authorization(authorizationParams)
-    .then((resultAuthorization) => {
-      expect(resultAuthorization.reference).to.equal(authorizationParams.reference);
-      expect(resultAuthorization.returnMessage).to.equal('Success.');
-      return resultAuthorization;
-    })
-    .then(capturaVenda)
-    .then(cancelaVenda)
-    .catch((err) => {
-      console.log('ERRO', err)
-    })
-  });
-})
+function cancelaVenda(resultCapture: CaptureResponse): Promise<RefundResponse> {
+  const cancelSaleParams = {
+    "amount": 2099
+  };
+  return erede.cancelSale(resultCapture.tid, cancelSaleParams)
+}
